9a34234ce672ecb5a62131995c716f055fe3e18b
Katariya jignesh <katariyajignesh266@gmail.com>
2025-03-31 20:41:39 +0530

    Update LoginActivity.java

--- app/src/main/java/com/example/mylogin/LoginActivity.java
+++ app/src/main/java/com/example/mylogin/LoginActivity.java
@@ -1,28 +1,128 @@
 package com.example.mylogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.TextView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginEmail, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnLogin = findViewById(R.id.btnLogin);
-        Button tvSignup = findViewById(R.id.tvSignup);
+        loginEmail = findViewById(R.id.etEmail);
+        loginPassword = findViewById(R.id.etPassword);
+        loginButton = findViewById(R.id.btnLogin);
+        signupRedirectText = findViewById(R.id.tvSignup);
 
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, MainProductActivity.class);
-            startActivity(intent);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!validateEmail() | !validatePassword()) {
+
+                } else {
+                    checkUser();
+                }
+            }
         });
 
-        tvSignup.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-            startActivity(intent);
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private boolean validateEmail() {
+        String val = loginEmail.getText().toString();
+        if (val.isEmpty()) {
+            loginEmail.setError("Email cannot be empty");
+            return false;
+        } else {
+            loginEmail.setError(null);
+            return true;
+        }
+    }
+
+
+    public Boolean validatePassword(){
+        String val = loginPassword.getText().toString();
+        if (val.isEmpty()) {
+            loginPassword.setError("Password cannot be empty");
+            return false;
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+
+    public void checkUser(){
+        String userEmail = loginEmail.getText().toString().trim();
+        String userPassword = loginPassword.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("Email").equalTo(userEmail);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()){
+
+                    loginEmail.setError(null);
+                    String passwordFromDB = snapshot.child(userEmail).child("password").getValue(String.class);
+
+                    if (passwordFromDB.equals(userPassword)) {
+                        loginEmail.setError(null);
+
+                        String FullNameFromDB = snapshot.child(userEmail).child("FullName").getValue(String.class);
+                        String emailFromDB = snapshot.child(userEmail).child("email").getValue(String.class);
+
+                        Intent intent = new Intent(LoginActivity.this, MainProductActivity.class);
+
+                        intent.putExtra("name", FullNameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("password", passwordFromDB);
+
+                        startActivity(intent);
+                    } else {
+                        loginPassword.setError("Invalid Credentials");
+                        loginPassword.requestFocus();
+                    }
+                } else {
+                    loginEmail.setError("User does not exist");
+                    loginEmail.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
     }
 }
